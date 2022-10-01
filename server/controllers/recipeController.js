ea1dd6f1307ecbedf6c428d2e581f9ca022e73d9
Mick <wang.mick12@gmail.com>
2022-10-01 11:28:59 -0400

    Create categories with MonboDB

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,12 +1,55 @@
+require("../models/database");
+const Category = require("../models/Category");
 
-
-/** 
+/**
  * GET /
  * Homepage
-*/
+ */
 
 //request and response
-exports.homepage = async(req, res) => {
-    //render index page
-    res.render('index', {title: 'My Recipes - Home'});
-}
+exports.homepage = async (req, res) => {
+  try {
+    //database query to grab categories
+    const limitNumber = 5;
+    const categories = await Category.find({}).limit(limitNumber);
+
+    //render index page and display categories
+    res.render("index", { title: "My Recipes - Home", categories });
+  } catch (error) {
+    res.status(500).send({message: error.message || "Error Occured"});
+  }
+};
+
+/**
+ * GET /categories
+ * Categories
+ */
+exports.byCategory = async (req, res) => {
+    try {
+      //database query to grab categories
+      const limitNumber = 20;
+      const categories = await Category.find({}).limit(limitNumber);
+  
+      //render index page and display categories
+      res.render("categories", { title: "View Recipes by Category", categories });
+    } catch (error) {
+      res.status(500).send({message: error.message || "Error Occured"});
+    }
+  };
+
+//insert data to database
+// async function insertDymmyCategoryData() {
+//   try {
+//     // data to insert
+//     await Category.insertMany([
+//       {
+//         name: "Drinks",
+//         image: "drinks.jpeg",
+//       },
+//     ]);
+//   } catch (error) {
+//     console.log("err", +error);
+//   }
+// }
+
+// insertDymmyCategoryData();
--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -4,4 +4,5 @@ const recipeController = require('../controllers/recipeController');
 
 // App Routes - list all pages to link to recipe controller
 router.get('/', recipeController.homepage);
+router.get('/categories', recipeController.byCategory);
 module.exports = router;
