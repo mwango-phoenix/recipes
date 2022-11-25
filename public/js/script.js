eccd548f87f47994c4b5727d074717f75a739a3d
Mick <wang.mick12@gmail.com>
2022-11-25 16:20:27 -0500

    Buttons to remove ingredients and steps from form

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,4 @@
+// Add text input for ingredients
 let addIngredientsBtn = document.getElementById('addIngredientsBtn');
 let ingredientList = document.querySelector('.ingredientList');
 let ingredientDiv = document.querySelectorAll('.ingredientDiv')[0];
@@ -9,6 +10,7 @@ addIngredientsBtn.addEventListener('click', function(){
   ingredientList.appendChild(newIngredients);
 });
 
+// Add text input for instructions
 let addStepBtn = document.getElementById('addStepBtn');
 let instructionList = document.querySelector('.instructionList');
 let instructionDiv = document.querySelectorAll('.instructionDiv')[0];
@@ -18,4 +20,18 @@ addStepBtn.addEventListener('click', function(){
   let input = newStep.getElementsByTagName('input')[0];
   input.value = '';
   instructionList.appendChild(newStep);
+});
+
+// remove text input for ingredients
+let rmvIngredientsBtn = document.getElementById('rmvIngredientsBtn');
+
+rmvIngredientsBtn.addEventListener('click', function(){
+  ingredientList.removeChild(ingredientList.lastElementChild);
+});
+
+// remove text input for instructions
+let rmvStepBtn = document.getElementById('rmvStepBtn');
+
+rmvStepBtn.addEventListener('click', function(){
+  instructionList.removeChild(instructionList.lastElementChild);
 });
